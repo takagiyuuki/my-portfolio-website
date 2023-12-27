cebcdffa5b1384eba3e21bce5d84c2fc33de8686
Yuuki Takagi <snow7777man@gmail.com>
2023-12-27 11:29:55 +0900

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Work } from '@/components/work';
 
 const testText = `3x9  12:34  3–8  +8+x
 (SEMI)PER[M]ANE{N}T
@@ -28,30 +29,29 @@ export default function Home() {
   return (
     <>
       <div className='text-8xl'>
-<p>3x9  12:34  3–8  +8+x</p>
-<p>`->` `-->` `--->` `=>` `==>`</p>
-<p>S@N  s@n  :-)  •Smile</p>
-<br />
-<p>Difficult affine fjord</p>
-<p>after affine art interface</p>
-<p>ff ffi fft ft fi tt tf df dt ff kf kt rf</p>
-<p>rt vf vt wf wt yf yt ¡¿What?!</p>
-<br />
-<p>0.45, 0.91. +0.08</p>
-<p>1.00; 9.44, −0.13</p>
-<p>0:00. 1.13; ~7.12</p>
-<br />
-<p>(Hello) [World] {9000}</p>
-<p>A@B  3 + 9 ≈ 12 * 1 → X</p>
-<br />
-<p>1234567890</p>
-<br />
-<p>WP0ACO9XSI1012O9</p>
-<p>Illegal busineß βeta</p>
-<br />
+        <p>3x9 12:34 3–8 +8+x</p>
+        <p>S@N s@n :-) •Smile</p>
+        <br />
+        <p>Difficult affine fjord</p>
+        <p>after affine art interface</p>
+        <p>ff ffi fft ft fi tt tf df dt ff kf kt rf</p>
+        <p>rt vf vt wf wt yf yt ¡¿What?!</p>
+        <br />
+        <p>0.45, 0.91. +0.08</p>
+        <p>1.00; 9.44, −0.13</p>
+        <p>0:00. 1.13; ~7.12</p>
+        <br />
+        <p>(Hello) [World] {9000}</p>
+        <p>A@B 3 + 9 ≈ 12 * 1 → X</p>
+        <br />
+        <p>1234567890</p>
+        <br />
+        <p>WP0ACO9XSI1012O9</p>
+        <p>Illegal busineß βeta</p>
+        <br />
         <p>I’m not, uhm “smol”</p>
-        </div>
-      <p className='text-xl font-semibold'>hello world</p>
+      </div>
+      <Work />
     </>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
