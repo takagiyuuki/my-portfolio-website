f066c8850ad73f09c62d67c6aaeb248e92f7033b
Yuuki Takagi <snow7777man@gmail.com>
2023-12-27 11:29:27 +0900

    update HamburgerMenu.tsx

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -20,10 +20,10 @@ export function HamburgerMenu() {
         <DropdownMenu.Portal>
           <DropdownMenu.Content
             className='
-          min-w-[220px] rounded-md 
-          p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]
-          backdrop-blur-xl will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade
-          data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade'
+          data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade 
+          data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade
+          min-w-[220px] rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]
+          backdrop-blur-xl will-change-[opacity,transform]'
           >
             <DropdownMenu.Label>Menu</DropdownMenu.Label>
             <DropdownMenu.Separator className='m-[5px] h-[1px] bg-black' />
