ec9a3cc54caf8c5b9d013146fca5e6ad22b79e26
Yuki Takagi <takagiyuuki@example.com>
2023-12-30 00:54:34 +0900

    update 1 file and create 2 files

--- src/components/animations.tsx
+++ src/components/animations.tsx
@@ -1,46 +1,27 @@
 import { YouTubeEmbed } from '@next/third-parties/google';
+import { LinkCard } from './LinkCard';
 
 export const Animations = () => {
   return (
     <div className='grid grid-cols-1 gap-1 lg:grid-cols-2'>
-      <VideoFlame videoid='M1Omy-1vPCs' />
-      <VideoFlame videoid='ZE4Q_ylGiwM' />
-      <VideoFlame videoid='wB6eNch9frQ' />
-      <VideoFlame videoid='1L57-Otwxqk' />
+      <YouTubeCard />
+      <YouTubeCard />
+      <YouTubeCard />
+      <YouTubeCard />
+      <YouTubeCard />
+      <YouTubeCard />
     </div>
   );
 };
 
-const VideoFlame = async ({ videoid }: Required) => {
+const YouTubeCard = () => {
+  let videoId = 'M1Omy-1vPCs';
+  let href = 'M1Omy-1vPCs';
+  console.log(href);
+  console.log(videoId);
   return (
-    <div className='py-4'>
-      <YouTubeEmbed videoid={videoid} />
-    </div>
+    <LinkCard>
+      <YouTubeEmbed videoid={videoId} />
+    </LinkCard>
   );
 };
-
-type Props = [
-  {
-    title: string;
-    id: string;
-  },
-];
-
-const VideoList: Object = [
-  { title: 'Sample1', id: 'sample1' },
-  { title: 'Sample2', id: 'sample2' },
-  { title: 'Sample3', id: 'sample3' },
-  { title: 'Sample4', id: 'sample4' },
-  { title: 'Sample5', id: 'sample5' },
-  { title: 'Sample6', id: 'sample6' },
-  { title: 'Sample7', id: 'sample7' },
-];
-
-// export const AnimationMap = VideoList.map((item, index) => {
-//   return (
-//     <div key={index}>
-//       <VideoFlame videoid={item.id} />
-//       <p>{item.title}</p>
-//     </div>
-//   );
-// });
---
+++ src/components/artwork.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { Grid } from './Grid';
+import { LinkCard } from './LinkCard';
+
+export const Artwork = () => {
+  return (
+    <Grid>
+      <LinkCard title='Test Image' desc='test brbrbr'>
+        <TestImage />
+      </LinkCard>
+      <LinkCard title='Test Image' desc='test brbrbr'>
+        <TestImage />
+      </LinkCard>
+      <LinkCard title='Test Image' desc='test brbrbr'>
+        <TestImage />
+      </LinkCard>
+      <LinkCard title='Test Image' desc='test brbrbr'>
+        <TestImage />
+      </LinkCard>
+      <LinkCard title='Test Image' desc='test brbrbr'>
+        <TestImage />
+      </LinkCard>
+    </Grid>
+  );
+};
+
+const TestImage = () => {
+  return <Image src='/next.svg' width={100} height={100} alt='test' />;
+};
