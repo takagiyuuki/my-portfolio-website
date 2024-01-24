48b387791c19a7af01c7a5211f4688f712570c08
Yuuki Takagi <snow7777man@gmail.com>
2024-01-24 15:04:15 +0900

    update test directory

--- src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-
-const markdown: string = '/test.md';
-
-export default function Test() {
-  return <ReactMarkdown remarkPlugins={{ remarkGfm }} url={markdown} />;
-}
