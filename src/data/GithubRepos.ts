436a5111221a5ba86571f114245f9e85b3a6b9c5
Yuuki Takagi <snow7777man@gmail.com>
2024-01-24 14:59:52 +0900

    feat: create GithubRepos.ts

---
+++ src/data/GithubRepos.ts
@@ -0,0 +1,10 @@
+export const GithubRepos = ['my-portfolio-website', 'python'];
+
+export const VideoIds = [
+  'ssssssssssssssss',
+  'ssssssssssssssss',
+  'ssssssssssssssss',
+  'ssssssssssssssss',
+  'ssssssssssssssss',
+  'ssssssssssssssss',
+];
