669b1ef501576e754cc59e3de25169911e666142
Yuuki Takagi <snow7777man@gmail.com>
2024-02-05 16:02:03 +0900

    update type.ts

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,18 +1,3 @@
 export type RootLayoutProps = {
   children: React.ReactNode;
 };
-
-export type ButtonProps = {
-  children?: React.ReactNode;
-  href: string | URL;
-  text?: string | null;
-  target?: string;
-  rel?: string;
-};
-
-export type LinkCardProps = {
-  title?: string | null;
-  desc?: React.ReactNode | null;
-  children?: React.ReactNode | null;
-  icon?: React.ReactNode | null;
-};
